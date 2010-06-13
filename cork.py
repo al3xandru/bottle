129dbce81815ee130eeabc25cea2b3efdfc6fe04
Alex Popescu <the.mindstorm@gmail.com>
2010-06-13 23:25:59 +0300

    Failed attempt to implement media-type negotiation as a simple bottle filter (led to infinite recursion issues)

---
+++ cork.py
@@ -0,0 +1,219 @@
+from bottle import request, response
+
+
+class MediaTypeFilter(object):
+  def __init__(self):
+    self.mediatype_filters = {}
+    self.types_mediatypes = {}
+    
+  def add_filter(self, func, ftype, media_types=[]):
+    ''' Register a new output filter for the given type and the media types. 
+    Whenever bottle hits a handler output matching `ftype` and content negotiation picks
+    a media type, `func` is applyed to it. 
+
+    ftype: return type to be matched
+    media_types: iterable of media types
+    func: function to be called
+    '''
+    if not isinstance(ftype, type):
+      raise TypeError("Expected type object, got %s" % type(ftype))
+    if not media_types:
+      print "WARNING: filters should declare their specific media types"
+    media_types = media_types or ['*/*']
+    self.types_mediatypes[ftype] = self.types_mediatypes.get(ftype) or set()
+    for mt in media_types:
+      if self.mediatype_filters.has_key((ftype, mt)):
+        print "WARNING: overriding filter %s for (%s, %s) with %s" % (self.mediatype_filters[(ftype, mt)].__name__, ftype.__name__, mt, func.__name__)
+      self.mediatype_filters[(ftype, mt)] = func
+      self.types_mediatypes[ftype].add(mt)
+      
+  def filter(self, value):
+    if not value:
+      response.headers['Content-Length'] = 0
+      if request.method != 'HEAD':
+        response.status = 204 if not response.status else response.status
+        return []
+      
+    if isinstance(value, (bottle.HTTPError, bottle.HTTPResponse)):
+      return value
+    
+    # Should return after setting the content type
+    if hasattr(value, 'read'):
+      supported = ['application/octet-stream']
+      if response.content_type:
+        supported.append(response.content_type)
+      content_type = self._best_media_match(supported, accepted_mediatypes)
+      if content_type and content_type != '*/*':
+        response.content_type = content_type
+      return value
+  
+    accepted_mediatypes = [parse_media_range(r) for r in request.header.get('accept', '*/*').split(",")]
+  
+    type_mtypes = {}
+    for ftype in self.types_mediatypes:
+      if isinstance(out, ftype):
+        for mt in self.types_mediatypes[ftype]:
+          type_mtypes[mt] = type_mtypes.get(mt, [])
+          type_mtypes[mt].append(ftype)
+    if type_mtypes:
+      content_type = self._best_media_match(type_mtypes.keys(), accepted_mediatypes)
+      if content_type:
+        types = type_mtypes[content_type]
+        types = sorted(types, cmp=lambda o1, o2: len(o2.__mro__) - len(o1.__mro__))
+        mtype = types[0]
+  
+        return self.filter(self.mediatype_filters[(mtype, content_type)](out))
+      
+    if isinstance(out, unicode):
+      out = out.encode(response.charset)
+      
+    if isinstance(out, bottle.StringType):
+      supported = ['text/plain', 'application/octet-stream']
+      if response.content_type:
+        supported.append(response.content_type)
+      response.content_type = self._best_media_match(supported, accepted_mediatypes) or \
+                   ("text/plain; charset=%s" % response.charset if response.charset else 'text/plain')
+      response.headers['Content-Length'] = str(len(out))
+      return [out]
+  
+    return self.filter(bottle.HTTPError(code=406, 
+            output="The requested URI '%s' exists, but not in a format preferred by the client." % request.path))
+
+  
+  def _best_media_match(self, supported, parsed_header):
+    """Takes a list of supported mime-types and finds the best
+    match for all the media-ranges listed in header. `parsed_header`
+    is the already parsed HTTP Accept header.
+    The value of 'supported' is a list of mime-types.
+
+    """
+    weighted_matches = [(fitness_and_quality_parsed(mime_type, parsed_header), mime_type)\
+                        for mime_type in supported]
+    weighted_matches.sort()
+    return weighted_matches[-1][0][1] and weighted_matches[-1][1] or ''
+
+
+"""MIME-Type Parser
+
+This module provides basic functions for handling mime-types. It can handle
+matching mime-types against a list of media-ranges. See section 14.1 of 
+the HTTP specification [RFC 2616] for a complete explanation.
+
+   http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+
+Contents:
+    - parse_mime_type():   Parses a mime-type into its component parts.
+    - parse_media_range(): Media-ranges are mime-types with wild-cards and a 'q' quality parameter.
+    - quality():           Determines the quality ('q') of a mime-type when compared against a list of media-ranges.
+    - quality_parsed():    Just like quality() except the second parameter must be pre-parsed.
+    - best_match():        Choose the mime-type with the highest quality ('q') from a list of candidates. 
+"""
+
+__version__ = "0.1.2"
+__author__ = 'Joe Gregorio'
+__email__ = "joe@bitworking.org"
+__credits__ = ""
+
+def parse_mime_type(mime_type):
+  """Carves up a mime-type and returns a tuple of the
+     (type, subtype, params) where 'params' is a dictionary
+     of all the parameters for the media range.
+     For example, the media range 'application/xhtml;q=0.5' would
+     get parsed into:
+
+     ('application', 'xhtml', {'q', '0.5'})
+     """
+  parts = mime_type.split(";")
+  params = dict([tuple([s.strip() for s in param.split("=")])\
+                 for param in parts[1:] ])
+  full_type = parts[0].strip()
+  # Java URLConnection class sends an Accept header that includes a single "*"
+  # Turn it into a legal wildcard.
+  if full_type == '*': full_type = '*/*'
+  (type, subtype) = full_type.split("/")
+  return (type.strip(), subtype.strip(), params)
+
+def parse_media_range(range):
+  """Carves up a media range and returns a tuple of the
+     (type, subtype, params) where 'params' is a dictionary
+     of all the parameters for the media range.
+     For example, the media range 'application/*;q=0.5' would
+     get parsed into:
+
+     ('application', '*', {'q', '0.5'})
+
+     In addition this function also guarantees that there 
+     is a value for 'q' in the params dictionary, filling it
+     in with a proper default if necessary.
+     """
+  (type, subtype, params) = parse_mime_type(range)
+  if not params.has_key('q') or not params['q'] or \
+     not float(params['q']) or float(params['q']) > 1\
+     or float(params['q']) < 0:
+    params['q'] = '1'
+  return (type, subtype, params)
+
+def fitness_and_quality_parsed(mime_type, parsed_ranges):
+  """Find the best match for a given mime-type against 
+     a list of media_ranges that have already been 
+     parsed by parse_media_range(). Returns a tuple of
+     the fitness value and the value of the 'q' quality
+     parameter of the best match, or (-1, 0) if no match
+     was found. Just as for quality_parsed(), 'parsed_ranges'
+     must be a list of parsed media ranges. """
+  best_fitness = -1 
+  best_fit_q = 0
+  (target_type, target_subtype, target_params) =\
+   parse_media_range(mime_type)
+  for (type, subtype, params) in parsed_ranges:
+    if (type == target_type or type == '*' or target_type == '*') and \
+       (subtype == target_subtype or subtype == '*' or target_subtype == '*'):
+      param_matches = reduce(lambda x, y: x+y, [1 for (key, value) in \
+                                                target_params.iteritems() if key != 'q' and \
+                                                params.has_key(key) and value == params[key]], 0)
+      fitness = (type == target_type) and 100 or 0
+      fitness += (subtype == target_subtype) and 10 or 0
+      fitness += param_matches
+      if fitness > best_fitness:
+        best_fitness = fitness
+        best_fit_q = params['q']
+
+  return best_fitness, float(best_fit_q)
+
+def quality_parsed(mime_type, parsed_ranges):
+  """Find the best match for a given mime-type against
+  a list of media_ranges that have already been
+  parsed by parse_media_range(). Returns the
+  'q' quality parameter of the best match, 0 if no
+  match was found. This function bahaves the same as quality()
+  except that 'parsed_ranges' must be a list of
+  parsed media ranges. """
+  return fitness_and_quality_parsed(mime_type, parsed_ranges)[1]
+
+def quality(mime_type, ranges):
+  """Returns the quality 'q' of a mime-type when compared
+  against the media-ranges in ranges. For example:
+
+  >>> quality('text/html','text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5')
+  0.7
+
+  """ 
+  parsed_ranges = [parse_media_range(r) for r in ranges.split(",")]
+  return quality_parsed(mime_type, parsed_ranges)
+
+def best_match(supported, header):
+  """Takes a list of supported mime-types and finds the best
+  match for all the media-ranges listed in header. The value of
+  header must be a string that conforms to the format of the 
+  HTTP Accept: header. The value of 'supported' is a list of
+  mime-types.
+
+  >>> best_match(['application/xbel+xml', 'text/xml'], 'text/*;q=0.5,*/*; q=0.1')
+  'text/xml'
+  """
+  parsed_header = [parse_media_range(r) for r in header.split(",")]
+  weighted_matches = [(fitness_and_quality_parsed(mime_type, parsed_header), mime_type)\
+                      for mime_type in supported]
+  weighted_matches.sort()
+  return weighted_matches[-1][0][1] and weighted_matches[-1][1] or ''
+
--- test/test_contentnegociation.py
+++ test/test_contentnegociation.py
@@ -2,6 +2,9 @@ import unittest
 
 import bottle
 from corktools import CorkedServerTest
+from tools import ServerTestBase
+
+from cork import MediaTypeFilter
 
 class ContentNegociation(CorkedServerTest):
   def testNoMediaType(self):
@@ -26,7 +29,7 @@ class ContentNegociation(CorkedServerTest):
     
     self.assertStatus(200, '/unicode')
     self.assertBody('unicode', '/unicode')
-    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/unicode')
+    self.assertHeader('Content-Type', 'text/plain', '/unicode')
     
     self.assertStatus(406, '/unicode', env={'HTTP_ACCEPT': 'application/xhtml'})
     
@@ -61,6 +64,83 @@ class ContentNegociation(CorkedServerTest):
     self.app.add_filter(A, lambda out: 'A', ['text/plain'])
     self.app.add_filter(B, lambda out: 'B', ['text/html'])
     self.app.add_filter(C, lambda out: 'C', ['text/plain'])
+    
+    @bottle.route('/b')
+    def r_b(): return B()
+    
+    @bottle.route('/c')
+    def r_c(): return C()
+    
+    self.assertStatus(200, '/b', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
+    self.assertBody('A', '/b', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
+    
+    self.assertBody('C', '/c', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
+    
+class StandardContentNegociation(ServerTestBase):
+  def testNoMediaType(self):
+    mtf = MediaTypeFilter()
+    self.app.add_filter(object, mtf.filter)
+    
+    @bottle.route('/null')
+    def null(): pass
+
+    self.assertStatus(204, '/null')
+    self.assertBody('', '/null')
+
+    @bottle.route('/error_return')
+    def error_return(): return bottle.HTTPError(code=404)
+    
+    self.assertStatus(404, '/error_return')
+    
+    @bottle.route('/error_raise')
+    def error_raise(): raise bottle.HTTPError(code=501)
+    
+    self.assertStatus(501, '/error_raise')
+    
+    @bottle.route('/unicode')
+    def r_unicode(): return u'unicode'
+    
+    self.assertStatus(200, '/unicode')
+    self.assertBody('unicode', '/unicode')
+    self.assertHeader('Content-Type', 'text/plain', '/unicode')
+    
+    self.assertStatus(406, '/unicode', env={'HTTP_ACCEPT': 'application/xhtml'})
+    
+    @bottle.route('/str')
+    def r_str(): return 'str'
+    
+    self.assertStatus(200, '/str')
+    self.assertBody('str', '/str')
+    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/str')    
+    
+  def testStaticFiles(self):
+    @bottle.route('/static_file')
+    def static(): return bottle.static_file('test_contentnegociation.py', root='.')
+
+    self.assertStatus(200, '/static_file')
+    self.assertHeader('Content-Type', 'text/x-python', '/static_file')
+    
+    #self.assertStatus(406, '/static_file', env={'HTTP_ACCEPT': 'application/xml'})
+    
+  def testFilters(self):
+    mtf = MediaTypeFilter()
+    mtf.add_filter(list_mapper, list, ['text/plain'])    
+    self.app.add_filter(object, mtf.filter)
+    
+    @bottle.route('/list')
+    def r_list(): return [1, 2, 3]
+    
+    self.assertStatus(200, '/list')
+    self.assertBody('list', '/list')
+    
+    #self.assertStatus(406, '/list', env={'HTTP_ACCEPT': 'application/xml'})
+    
+  def testAdvancedFilters(self):
+    mtf = MediaTypeFilter()
+    mtf.add_filter(lambda out: 'A', A, ['text/plain'])
+    mtf.add_filter(lambda out: 'B', B, ['text/html'])
+    mtf.add_filter(lambda out: 'C', C,  ['text/plain'])
+    self.app.add_filter(object, mtf.filter)
     
     @bottle.route('/b')
     def r_b(): return B()
