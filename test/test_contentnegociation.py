3d1507324d756264c6faf1f1c1848f41c193febd
Alex Popescu <the.mindstorm@gmail.com>
2010-06-14 03:28:01 +0300

    Completed implementation for content negotiation (media types)

--- test/test_contentnegociation.py
+++ test/test_contentnegociation.py
@@ -4,23 +4,37 @@ import bottle
 from corktools import CorkedServerTest
 
 class ContentNegociation(CorkedServerTest):
-  def testNoMediaType(self):
+  def testEmptyReturn(self):
     @bottle.route('/null')
-    def null(): pass
-
+    def r_null(): pass
+    
     self.assertStatus(204, '/null')
     self.assertBody('', '/null')
 
+    self.assertStatus(200, '/null', method='HEAD')
+    
+    @bottle.route('/null-post', method='POST')
+    def r_null(): pass
+    
+    self.assertStatus(405, '/null-post', method='HEAD')
+    
+  def testHTTPError(self):
     @bottle.route('/error_return')
     def error_return(): return bottle.HTTPError(code=404)
     
     self.assertStatus(404, '/error_return')
+    self.assertHeader('Content-Type', 'text/html; charset=UTF-8', '/error_return')
+    self.assertHeader('Content-Type', 'text/html; charset=UTF-8', '/error_return', env={'HTTP_ACCEPT': 'text/html; q=0.5, text/plain; q=0.9'})
     
     @bottle.route('/error_raise')
     def error_raise(): raise bottle.HTTPError(code=501)
     
     self.assertStatus(501, '/error_raise')
+    self.assertHeader('Content-Type', 'text/html; charset=UTF-8', '/error_return')
+    self.assertHeader('Content-Type', 'text/html; charset=UTF-8', '/error_return', env={'HTTP_ACCEPT': 'text/html; q=0.5, text/plain; q=0.9'})    
     
+    
+  def testNoMediaType(self):
     @bottle.route('/unicode')
     def r_unicode(): return u'unicode'
     
@@ -28,7 +42,13 @@ class ContentNegociation(CorkedServerTest):
     self.assertBody('unicode', '/unicode')
     self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/unicode')
     
-    self.assertStatus(406, '/unicode', env={'HTTP_ACCEPT': 'application/xhtml'})
+    # http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.7
+    # HTTP/1.1 servers are allowed to return responses which are
+    #  not acceptable according to the accept headers sent in the
+    #  request. In some cases, this may even be preferable to sending a
+    #  406 response.
+    self.assertStatus(200, '/unicode', env={'HTTP_ACCEPT': 'application/xhtml'})
+    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/unicode')
     
     @bottle.route('/str')
     def r_str(): return 'str'
@@ -44,22 +64,29 @@ class ContentNegociation(CorkedServerTest):
     self.assertStatus(200, '/static_file')
     self.assertHeader('Content-Type', 'text/x-python', '/static_file')
     
-    #self.assertStatus(406, '/static_file', env={'HTTP_ACCEPT': 'application/xml'})
+    # http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.7
+    self.assertStatus(200, '/static_file', env={'HTTP_ACCEPT': 'application/xml'})
+    self.assertHeader('Content-Type', 'text/x-python', '/static_file')
     
   def testFilters(self):
-    self.app.add_filter(list, list_mapper, ['text/plain'])
+    self.app.add_filter(list, lambda out: 'list', ['text/plain'])
     
     @bottle.route('/list')
     def r_list(): return [1, 2, 3]
     
     self.assertStatus(200, '/list')
     self.assertBody('list', '/list')
+    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/list')
     
-    #self.assertStatus(406, '/list', env={'HTTP_ACCEPT': 'application/xml'})
+    self.assertStatus(406, '/list', env={'HTTP_ACCEPT': 'application/xml'})
     
   def testAdvancedFilters(self):
+    def to_b(out):
+      from bottle import response
+      response.content_type = 'text/html'
+      return 'B'
     self.app.add_filter(A, lambda out: 'A', ['text/plain'])
-    self.app.add_filter(B, lambda out: 'B', ['text/html'])
+    self.app.add_filter(B, to_b, ['text/html'])
     self.app.add_filter(C, lambda out: 'C', ['text/plain'])
     
     @bottle.route('/b')
@@ -68,14 +95,20 @@ class ContentNegociation(CorkedServerTest):
     @bottle.route('/c')
     def r_c(): return C()
     
-    self.assertStatus(200, '/b', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
-    self.assertBody('A', '/b', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
+    ACCEPT_HEADERS = {'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'}
+    self.assertStatus(200, '/b', env=ACCEPT_HEADERS)
+    self.assertBody('A', '/b', env=ACCEPT_HEADERS)
+    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/b', env=ACCEPT_HEADERS)
     
-    self.assertBody('C', '/c', env={'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'})
+    ACCEPT_HEADERS = {'HTTP_ACCEPT': ' text/html, text/plain; q=0.9'}
+    self.assertStatus(200, '/b', env=ACCEPT_HEADERS)
+    self.assertBody('B', '/b', env=ACCEPT_HEADERS)
+    self.assertHeader('Content-Type', 'text/html', '/b', env=ACCEPT_HEADERS)
+    
+    ACCEPT_HEADERS = {'HTTP_ACCEPT': 'text/plain, text/html; q=0.8'}
+    self.assertBody('C', '/c', env=ACCEPT_HEADERS)
+    self.assertHeader('Content-Type', 'text/plain; charset=UTF-8', '/c', env=ACCEPT_HEADERS)
                         
-def list_mapper(out):
-  return "list"
-
 class A(object): pass
 
 class B(A): pass
